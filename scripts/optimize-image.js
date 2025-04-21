22e93fe594f0457f642adf2c1c85f1f34285d787
antonmarshall <anton.marshall03@gmail.com>
2025-04-21 22:44:49 +0200

    Deploy to GitHub Pages

---
+++ scripts/optimize-image.js
@@ -0,0 +1,26 @@
+import sharp from 'sharp';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { renameSync } from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const inputPath = join(__dirname, '../public/images/profilbild.png');
+const outputPath = join(__dirname, '../public/images/profilbild.png');
+
+sharp(inputPath)
+  .resize(800, 800, {
+    fit: 'inside',
+    withoutEnlargement: true
+  })
+  .jpeg({
+    quality: 80,
+    progressive: true
+  })
+  .toFile(outputPath + '.temp')
+  .then(info => {
+    renameSync(outputPath + '.temp', outputPath);
+    console.log('Bild wurde optimiert:', info);
+  })
+  .catch(err => console.error('Fehler bei der Bildoptimierung:', err)); 
