d25cc47abc7a7aae65d1865ef595882660768821
antonmarshall <anton.marshall03@gmail.com>
2025-04-23 10:07:36 +0200

    Update background color and hero image

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -33,6 +33,13 @@ const HeroSection = () => {
           </div>
         </div>
       </div>
+      <div className="relative w-full h-[500px] md:h-[600px] overflow-hidden">
+        <img
+          src="/webseite-andrea-wennecke/images/hero-image.jpg"
+          alt="Andrea Wennecke - Kreativtherapie"
+          className="w-full h-full object-cover"
+        />
+      </div>
     </section>
   );
 };
